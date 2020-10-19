b1facb59b32b1e757c286c3f6855f5643c539122
Yoann Brosseau <yoann.brosseau@gmail.com>
2020-10-19 08:27:38 +0200

    init

---
+++ config.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import yaml
+
+
+def get_sizes(maximum_size):
+    if not maximum_size:
+        maximum_size = 'HD'
+
+    if maximum_size == 'VGA':  # 640x480
+        return ["url_z"]
+    elif maximum_size == 'SVGA':  # 800x600
+        return ["url_c", "url_z"]
+    elif maximum_size == 'XGA':  # 1024x768
+        return ["url_b", "url_c", "url_z"]
+    elif maximum_size == 'UXGA':  # 1600x1200
+        return ["url_h", "url_b", "url_c"]
+    elif maximum_size == 'HD':  # 1920x1080
+        return ["url_k", "url_o", "url_h", "url_l"]
+    elif maximum_size == '4K':  # 4096
+        return ["url_f", "url_4k", "url_o", "url_3k", "url_k", "url_h"]
+    elif maximum_size == '6K':  # 6144
+        return ["url_6k", "url_5k", "url_o", "url_f", "url_4k", "url_3k", "url_k", "url_h"]
+
+
+class Config(object):
+    data = dict(
+        api_key='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx',
+        api_secret='XXXXXXXXXXXXX',
+        max_image_pull=50,
+        image_ratio=1.333333,
+        horizontal=True,
+        maximum_size='HD',
+        delay=60
+    )
+    api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx'
+    api_secret = 'XXXXXXXXXXXXX'
+    max_image_pull = 50
+    image_ratio = 1.333333
+    horizontal = True
+    maximum_size = 'HD'
+    delay = 60
+    sizes = ["url_k", "url_o", "url_h", "url_l"]
+
+    def __init__(self):
+
+        if getattr(sys, 'frozen', False):
+            self.base_dir = os.path.dirname(sys.executable)
+        elif __file__:
+            self.base_dir = os.path.dirname(__file__)
+        self.config_file = os.path.join(self.base_dir, 'config.yml')
+
+    def load_config(self):
+        if os.path.exists(self.config_file):
+            with open(self.config_file, 'r', encoding='UTF-8') as f:
+                cur_config = yaml.safe_load(f)
+
+            self.api_key = cur_config.get('api_key')
+            self.api_secret = cur_config.get('api_secret')
+            self.max_image_pull = cur_config.get('max_image_pull')
+            self.image_ratio = cur_config.get('image_ratio')
+            self.horizontal = cur_config.get('horizontal')
+            self.maximum_size = cur_config.get('maximum_size')
+            self.sizes = get_sizes(self.maximum_size)
+            self.delay = cur_config.get('delay')
+            return True
+        else:
+            self.save_config()
+            return False
+
+    def save_config(self):
+        self.data['api_key'] = self.api_key
+        self.data['api_secret'] = self.api_secret
+        self.data['max_image_pull'] = self.max_image_pull
+        self.data['image_ratio'] = self.image_ratio
+        self.data['horizontal'] = self.horizontal
+        self.data['maximum_size'] = self.maximum_size
+        self.data['delay'] = self.delay
+
+        with open(self.config_file, 'w', encoding='UTF-8') as f:
+            yaml.safe_dump(self.data, f)
+
+
+config = Config()
---
+++ flickr.py
@@ -0,0 +1,84 @@
+import os.path
+import requests
+from random import randrange
+
+from flickrapi import FlickrAPI
+
+from config import config
+
+MIN_SIZE_RATIO = 1.333333
+
+
+def get_photos():
+    extras = ','.join(config.sizes)
+    flickr = FlickrAPI(config.api_key, config.api_secret, cache=True)
+    photos = flickr.interestingness.getList(extras=extras,  # get the urls for each size we want
+                                            privacy_filter=1,  # search only for public photos
+                                            per_page=config.max_image_pull,
+                                            sort='relevance')
+    return photos[0]
+
+
+def get_url(sel_photo, min_size_ratio=None):
+    if not min_size_ratio:
+        min_size_ratio = MIN_SIZE_RATIO
+    attrib = sel_photo.attrib
+    for i in range(len(config.sizes)):  # makes sure the loop is done in the order we want
+        purl = attrib.get(config.sizes[i])
+        if purl:  # if url is None try with the next size
+            suffix = config.sizes[i].split('_')[1]
+            height_str = 'height_' + suffix
+            width_str = 'width_' + suffix
+            height = attrib.get(height_str)
+            width = attrib.get(width_str)
+            ratio = int(width) / int(height)
+
+            if config.horizontal:
+                if ratio > min_size_ratio:
+                    return purl
+            else:
+                if ratio < min_size_ratio:
+                    return purl
+
+
+def download_images(urls, path):
+    for purl in urls:
+        download_image(purl, path)
+
+
+def download_image(url, path):
+    image_name = url.split("/")[-1]
+    image_path = os.path.join(path, image_name)
+
+    if not os.path.isfile(image_path):  # ignore if already downloaded
+        response = requests.get(url, stream=True)
+
+        with open(image_path, 'wb') as outfile:
+            outfile.write(response.content)
+
+        return image_path
+
+
+def get_urls(max_url=10000, min_size_ratio=None):
+    photos = get_photos()
+    counter = 0
+    urls = []
+
+    for photo in photos:
+        if counter < max_url:
+            url = get_url(photo, min_size_ratio)  # get preffered size url
+            if url:
+                urls.append(url)
+                counter += 1
+            # if no url for the desired sizes then try with the next photo
+        else:
+            break
+
+    return urls
+
+
+def get_random_url():
+    urls = get_urls(min_size_ratio=config.image_ratio)
+    total = len(urls)
+    num = randrange(total)
+    return urls[num]
